f7ec5beaf524412f90d7f5cfcb3a4c3aa717fd54
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-03-20 03:25:22 -0300

    Little text change

--- src/tabs/summary/summary.tsx
+++ src/tabs/summary/summary.tsx
@@ -26,49 +26,49 @@ export default function Summary() {
   return (
     <div className="h-full flex flex-col items-center justify-around gap-14 py-[44px]">
         <div className="bg-bg-secondary rounded-lg p-5 w-full ">
-          <h2 className="text-lg xl:text-xxl font-bold mb-4 text-center xl:text-start">
+          <h2 className="text-lg xl:text-xxl font-medium mb-4 text-center xl:text-start">
             Datos de entrada
           </h2>
           <div className="grid center grid-cols-1 md:grid-cols-3 xl:grid-cols-7 grid-rows-3 xl:grid-rows-1 gap-4 text-center">
             <div className="bg-primary/20 py-6 xxl:px-4 rounded-lg ">
               <p className="text-main-color text-md">Nombre</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 {localCommission?.name || "Vacío"}
               </p>
             </div>
             <div className="bg-primary/20 py-6 px-4 rounded-lg">
               <p className="text-main-color text-md">Mes</p>
-              <p className="text-md font-bold self-center">
+              <p className="text-md font-medium self-center">
                 {localCommission?.month || "Vacío"}
               </p>
             </div>
             <div className="bg-primary/20 py-6 px-4 rounded-lg">
               <p className="text-main-color text-md">Producto</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 {localCommission?.product || "Vacío"}
               </p>
             </div>
             <div className="bg-primary/20 py-6 xxl:px-4 rounded-lg">
               <p className="text-main-color text-md">Valor del producto</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 ${localCommission?.productValue?.toLocaleString() || 0}
               </p>
             </div>
             <div className="bg-primary/20 py-6 px-4 rounded-lg">
               <p className="text-main-color text-md">Comisión</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 %{localCommission?.commission || 0}
               </p>
             </div>
             <div className="bg-primary/20 py-6 px-4 rounded-lg">
               <p className="text-main-color text-md">Valor USD</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 ${localCommission?.usdValue?.toLocaleString() || 0}
               </p>
             </div>
             <div className="bg-primary/20 py-6 px-4 rounded-lg">
               <p className="text-main-color text-md">Ticket USD</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 ${localCommission?.usdTicket?.toLocaleString() || 0}
               </p>
             </div>
@@ -77,29 +77,29 @@ export default function Summary() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center justify-around gap-10 w-full">
         <div className="bg-bg-secondary rounded-lg p-6 h-fit">
-          <h2 className="text-lg xl:text-xl font-bold mb-4 text-center xl:text-start">Objetivos</h2>
+          <h2 className="text-lg xl:text-xl font-medium mb-4 text-center xl:text-start">Objetivos</h2>
           <div className="grid grid-cols-1 grid-rows-4 xxl:grid-rows-3 gap-4">
-            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 block text-center xl:flex items-center gap-3 ">
+            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 block text-center xl:flex items-center gap-2 ">
               <p className="text-main-color text-sm">Ganancia neta</p>
-              <p className="text-md font-bold underline decoration-green-400 decoration-1 underline-offset-4">
+              <p className="text-md font-medium underline decoration-green-400 decoration-1 underline-offset-4">
                 ${localGoal?.netProfit?.toLocaleString() || 0}
               </p>
             </div>
-            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 row-start-2 block text-center xl:flex items-center gap-3">
+            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 row-start-2 block text-center xl:flex items-center gap-2">
               <p className="text-main-color text-sm">Tengo que vender (PESOS)</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 ${localGoal?.needToSell?.toLocaleString() || 0}
               </p>
             </div>
-            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 block text-center xl:flex items-center gap-3">
+            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 block text-center xl:flex items-center gap-2">
               <p className="text-main-color text-sm">Ventas mensuales</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 {localGoal?.monthSales?.toFixed(0) || 0}
               </p>
             </div>
-            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 block text-center xl:flex items-center gap-3">
+            <div className="bg-primary/20 py-3 px-4 rounded-lg col-span-2 block text-center xl:flex items-center gap-2">
               <p className="text-main-color text-sm">Volumen en carrera (USD)</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 ${localGoal?.runVolume?.toLocaleString() || 0}
               </p>
             </div>
@@ -107,27 +107,27 @@ export default function Summary() {
         </div>
 
         <div className="bg-bg-secondary rounded-lg p-6 h-full items-start flex flex-col">
-          <h2 className="text-lg xl:text-xxl font-bold mb-4 text-center xl:text-start">
+          <h2 className="text-lg xl:text-xxl font-medium mb-4 text-center xl:text-start">
             Prospectar datos
           </h2>
           <div className="grid grid-cols-1 grid-rows-3 gap-4 w-full">
-            <div className="bg-primary/20 py-4.5 px-4 rounded-lg block text-center xl:flex gap-3 items-center">
+            <div className="bg-primary/20 py-4.5 px-4 rounded-lg block text-center xl:flex gap-2 items-center">
               <p className="text-main-color text-sm">Nuevos datos a prospectar</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 {localProspect?.newDataToProspect || 0}
               </p>
             </div>
-            <div className="bg-primary/20 py-4.5 px-4 rounded-lg block text-center xl:flex gap-3 items-center">
+            <div className="bg-primary/20 py-4.5 px-4 rounded-lg block text-center xl:flex gap-2 items-center">
               <p className="text-main-color text-sm">Min. Presentaciónes (Mes)</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 {localProspect?.minPresentationsMonth?.toFixed(0) || 0}
               </p>
             </div>
-            <div className="bg-primary/20 py-4.5 px-4 rounded-lg block text-center xl:flex gap-3 items-center">
+            <div className="bg-primary/20 py-4.5 px-4 rounded-lg block text-center xl:flex gap-2 items-center">
               <p className="text-main-color text-sm">
                 Min. Presentaciónes (Semana)
               </p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 {localProspect?.minPresentationsWeek || 0}
               </p>
             </div>
@@ -136,23 +136,23 @@ export default function Summary() {
 
         <div className="flex flex-col items-center justify-around gap-4 h-full bg-bg-secondary rounded-lg">
           <div className="p-6 w-full block text-center xl:flex items-center justify-center">
-            <h2 className="text-lg xl:text-xxl font-bold xl:mb-0 mb-4 text-center xl:w-1/2">
+            <h2 className="text-lg xl:text-xxl font-medium xl:mb-0 mb-4 text-center xl:w-1/2">
               Objetivo principal
             </h2>
             <div className="bg-primary/20 p-2 rounded-lg xl:w-1/2 ">
               <p className="text-main-color text-sm">Objetivo a vender</p>
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 ${localGoal?.profitGoal?.toLocaleString() || 0}
               </p>
             </div>
           </div>
           <div className="h-1 w-4/5 bg-primary/30"></div>
-          <div className="bg-bg-secondary rounded-lg p-6 w-full block text-center xl:flex items-center gap-3">
-            <h2 className="text-lg xl:text-xxl xl:w-1/2 font-bold xl:mb-0 mb-4 text-center">
+          <div className="bg-bg-secondary rounded-lg p-6 w-full block text-center xl:flex items-center gap-2">
+            <h2 className="text-lg xl:text-xxl xl:w-1/2 font-medium xl:mb-0 mb-4 text-center">
               Capacitación mínima entre campus virtual y oficina
             </h2>
             <div className="bg-primary/20 p-2 rounded-lg xl:w-1/2">
-              <p className="text-md font-bold">
+              <p className="text-md font-medium">
                 20Hs semanales
               </p>
             </div>
